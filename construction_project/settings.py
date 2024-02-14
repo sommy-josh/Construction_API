ee470a8050f4e67ba1010b6218e2e4def1ddecc7
sommy-josh <chisomzzy1@>
2024-02-14 03:00:15 +0100

    pointed the allowed host to render

--- construction_project/settings.py
+++ construction_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(lmtj3r$^34ydcm^l&x0e=!da-wui%@#1w%g(o-masp^oir#q0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://constructionbuddy.onrender.com']
 
 
 # Application definition
