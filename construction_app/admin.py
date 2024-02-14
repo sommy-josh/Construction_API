7e852903978dd98428d1262221cc4c5ac2e46d37
sommy-josh <chisomzzy1@>
2024-02-14 03:33:13 +0100

    ...

--- construction_app/admin.py
+++ construction_app/admin.py
@@ -3,9 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import Contact,Home,GetQUote
 
-admin.site.site_header('Construction Admin')
-
-
 class HomeAdmin(admin.ModelAdmin):
     inlines = ['text','image']
 
