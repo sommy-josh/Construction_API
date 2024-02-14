4f5ceb2db62b3bbb6ae59932c2b11cf7d5f252c9
sommy-josh <chisomzzy1@>
2024-02-14 21:34:13 +0100

    bug-fix

--- construction_app/admin.py
+++ construction_app/admin.py
@@ -3,9 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Contact,Home,GetQUote
 
-class HomeAdmin(admin.ModelAdmin):
-    inlines = ['text','image']
 
-admin.site.register(Home,HomeAdmin),
+admin.site.register(Home),
 admin.site.register(GetQUote),
 admin.site.register(Contact),
--- construction_project/settings.py
+++ construction_project/settings.py
@@ -146,6 +146,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
-    "http://example.com",
-    "https://example.com",
 ]
